4a2445e03743ebffe4243c99831e6d891ea42f53
stellakaniaru <stellakaniaru16@gmail.com>
2019-10-30 14:33:51 +0300

    Code submission for first assignment

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello World")
+# declare variables
+name = "Stan"
+age = 20
+numbers = [12, 4, 56, 17, 8, 99]
+alphabet = {"A" : "Apple", "B" : "Boy", "C" : "Cow", "D" : 'Dog', "E" : "Elephant", "F" : "Fish", "G" : "Ghost",
+"H" : "Horse", "I" : "Icecream", "J" : "Jack", "K" : "Kangaroo", "L" : "Lamb", "M" : "Moon", "N" : "Nine", 
+"O" : "Owl", "P" : "Pig", "Q" : "Queen", "R" : "Ruler", "S" : "Schoolbag", "T" : "Train", "U" : "Umbrella",
+"V" : "Violet", "W" : "Wind", "X" : "Xylophone", "Y" : "Yo-yo", "Z" : "Zebra"}
+
+#print hello world
+print("Hello World\n")
+
+#print name and age
+print("My name is {}.".format(name))
+print("I am {}+ years of age. Young, right? 😃\n".format(age))
+
+#print calculations
+print("The maximum number in this list: {} is {}.".format(numbers, max(numbers)))
+print("The mean: {} is {}.\n".format(numbers, sum(numbers) / len(numbers)))
+
+#print alphabets
+for i in alphabet:
+	print("{} for {}".format(i, alphabet[i]))
+
+
